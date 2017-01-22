ad6563ef9f28bb1147a3d2b18a2780a4859a855c
K <hpkelvin@163.com>
2017-01-22 18:26:17 +0800

    Environment is basically finished, and is able to generate fingerprints. File structure refactoring. An experimental MapView.

--- app/src/main/java/ecnu/cs14/garagelocation/MainActivity.java
+++ app/src/main/java/ecnu/cs14/garagelocation/MainActivity.java
@@ -1,13 +1,52 @@
 package ecnu.cs14.garagelocation;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import ecnu.cs14.garagelocation.env.Environment;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = MainActivity.class.getName();
+
+    private MapView mapView;
+    private Environment environment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapView = (MapView) findViewById(R.id.map_view);
+        environment = Environment.getInstance(this);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: started");
+        final Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                HashMap fingerprint = (HashMap) msg.obj;
+                mapView.setText(fingerprint.toString());
+            }
+        };
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List<String> aps = environment.getAps();
+                HashMap<String, Integer> fingerprint = environment.generateFingerprint(new HashSet<>(aps));
+                Message message = new Message();
+                message.obj = fingerprint;
+                handler.sendMessage(message);
+            }
+        }).start();
+    }
+
 }
---
+++ app/src/main/java/ecnu/cs14/garagelocation/env/Wifi.java
@@ -0,0 +1,64 @@
+package ecnu.cs14.garagelocation.env;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+import java.lang.ref.WeakReference;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * Handles wifi related affairs.
+ * Provides scan results.
+ * Created by K on 16/12/11.
+ */
+
+final class Wifi extends BroadcastReceiver {
+    private final static String TAG = Wifi.class.getName();
+    private Context mContext;
+    private WifiManager mManager;
+    private HashSet<WeakReference<ScanResultsReceiver>> mCallbacks;
+
+    interface ScanResultsReceiver {
+        void receive(List<ScanResult> results);
+    }
+
+    Wifi(Context context) {
+        mContext = context;
+        mManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        mCallbacks = new HashSet<>();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "onReceive: started");
+        List<ScanResult> list = mManager.getScanResults();
+        for (WeakReference<ScanResultsReceiver> r :
+                mCallbacks) {
+            r.get().receive(list);
+        }
+        mContext.unregisterReceiver(this);
+    }
+
+    void enable() {
+        mManager.setWifiEnabled(true);
+    }
+
+    void scan() {
+        Log.i(TAG, "scan: started");
+        if (!mManager.isWifiEnabled()) {
+            enable();
+        }
+        mContext.registerReceiver(this, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        mManager.startScan();
+    }
+
+    void registerScanResultsReceiver(ScanResultsReceiver receiver) {
+        mCallbacks.add(new WeakReference<>(receiver));
+    }
+}
