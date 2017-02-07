05bac26a0a356b3cc21b149fdb4d7dc6c5339942
K <hpkelvin@163.com>
2017-02-07 17:25:27 +0800

    MapView is able to display a map.

--- app/src/main/java/ecnu/cs14/garagelocation/MapView.java
+++ app/src/main/java/ecnu/cs14/garagelocation/MapView.java
@@ -4,9 +4,14 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import ecnu.cs14.garagelocation.data.Map;
+
+import java.util.HashSet;
 
 /**
  * An experimental MapView.
@@ -17,6 +22,8 @@ public class MapView extends View {
     private final static String TAG = MapView.class.getName();
 
     private Paint mPaint;
+    private Paint mBackgroundPaint;
+    private View mEmptyView;
 
     public MapView(Context context) {
         super(context);
@@ -28,17 +35,52 @@ public class MapView extends View {
         initPaint();
     }
 
-    private void initPaint() {
-        mPaint = new Paint();
-        mPaint.setColor(Color.BLACK);
-        mPaint.setTextSize(48);
+    public void setEmptyView(@NonNull View v) {
+        mEmptyView = v;
     }
 
-    private String mString;
+    @Override
+    public void setVisibility(int visibility) {
+        super.setVisibility(visibility);
+        if (visibility != VISIBLE) {
+            mEmptyView.setVisibility(VISIBLE);
+        } else {
+            mEmptyView.setVisibility(GONE);
+        }
+    }
 
-    public void setText(String string) {
-        Log.i(TAG, "setText: String set:" + string);
-        mString = string;
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.DKGRAY);
+
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(Color.LTGRAY);
+    }
+
+    private Rect mBackgroundRect = new Rect();
+    private HashSet<Map.Shape.Rect> mRects = new HashSet<>();
+    private HashSet<Map.Shape.Circle> mCircles = new HashSet<>();
+    public void setMap(Map map) {
+        mBackgroundRect = new Rect(
+                0,
+                0,
+                map.width,
+                map.height
+        );
+        for (Map.Shape shape: map.shapes) {
+            switch (shape.type) {
+                case RECT:
+                {
+                    mRects.add((Map.Shape.Rect) shape);
+                    break;
+                }
+                case CIRCLE:
+                {
+                    mCircles.add((Map.Shape.Circle) shape);
+                    break;
+                }
+            }
+        }
         invalidate();
     }
 
@@ -55,9 +97,23 @@ public class MapView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Log.i(TAG, "onDraw: left: " + getLeft() + " top: " + getTop());
-        if (null != mString) {
-            canvas.drawText(mString, getLeft(), getTop() + 48 * 2, mPaint);
+        float sx = (float) getWidth() / (float) mBackgroundRect.right;
+        float sy = (float) getHeight() / (float) mBackgroundRect.bottom;
+        float s = sx < sy ? sx : sy;
+        canvas.translate(
+                (getWidth() / 2f - mBackgroundRect.centerX() * s),
+                (getHeight() / 2f - mBackgroundRect.centerY() * s)
+        );
+        canvas.scale(s, s, 0, 0);
+        canvas.clipRect(mBackgroundRect);
+        canvas.drawRect(mBackgroundRect, mBackgroundPaint);
+        for (Map.Shape.Rect rect :
+                mRects) {
+            canvas.drawRect(rect.left, rect.top, rect.right, rect.bottom, mPaint);
+        }
+        for (Map.Shape.Circle circle :
+                mCircles) {
+            canvas.drawCircle(circle.center_left, circle.center_top, circle.radius, mPaint);
         }
     }
 }
